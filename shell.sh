46826abdf690b96f378e02e193d22482de38ce97
paco <pacoson.wang@gmail.com>
2023-06-05 19:06:50 +0800

    优化脚本

--- shell.sh
+++ shell.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
-
 echo "进入脚本"
 
 if [ ! -n "$1" ] ;then
     echo "you have not input commit messsage!"
     exit 0
 else
-    echo "the word you input is $1"
+    echo "开始部署hexo"
 fi
 
-echo "开始部署hexo"
 hexo clean
 hexo g
 hexo d
